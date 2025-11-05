cdaf47466697f69a53ce6febab1a16ca2a5f7a03
sepehr <sepehr.frlncr@gmail.com>
2025-11-05 18:32:11 +0330

    add a weather app

--- app/components/sample-project/projectData.tsx
+++ app/components/sample-project/projectData.tsx
@@ -3,24 +3,32 @@ import admin from '../../../public/project`s Image/admin.png'
 import user from '../../../public/project`s Image/user.png'
 import dashbord from '../../../public/project`s Image/dashbord.png'
 import panel from '../../../public/project`s Image/panel.png'
+import weather from '../../../public/project`s Image/weather-app.png'
 
 export const ProjectData = [
-     {
+    {
         id : 1,
+        Image : weather,
+        title : " برنامه اب و هوا",
+        link: "https://weather-app-puce-eight-47.vercel.app/login",
+        category : 'توسعه وب'
+    },
+     {
+        id : 2,
         Image : panel,
         title : "پنل ادمین",
         link: "https://admin-panel-gamma-orcin.vercel.app/",
         category : 'Dashbord'
     },
     {
-        id : 2,
+        id : 3,
         Image : login,
         title : "فرم ثبت نام",
         link: "project/login",
         category : 'Component'
     },
     {
-        id : 3,
+        id : 4,
         Image : admin,
         title : "داشبورد ادمین",
         link: "project/admin",
@@ -28,14 +36,14 @@ export const ProjectData = [
 
     },
     {
-        id : 4,
+        id : 5,
         Image : user,
         title : "داشبورد کاربر",
         link: "project/user",
         category : 'Dashbord'
     },
     {
-        id : 5,
+        id : 6,
         Image : dashbord,
         title : "داشبورد شیشه ای",
         link: "project/dashbord",
